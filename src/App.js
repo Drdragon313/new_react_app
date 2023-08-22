0541e1272ce208bf59481f1b37736a9cab50afcd
Drdragon313 <muhammadahmad742@gmail.com>
2023-08-22 15:29:21 +0500

    second commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ function App() {
   return (
     <div className="App">
     <h3>This is Muhammad Ahmed's first ReactJs app in 31G as a project</h3>
+    <h3>github update done</h3>
     </div>
   );
 }
